bbb7fdd34104e347a4f67d16312ae166ca62bb7a
crisen06 <crisen.cordova@gmail.com>
2026-05-05 23:54:49 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "vnet_name" {
-  value = azurerm_virtual_network.vnet_utec.name
-}
-
-output "vnet_cidr" {
-  value = azurerm_virtual_network.vnet_utec.address_space[0]
-}
-
-output "subnet_id" {
-  value = azurerm_subnet.subnet_privada.id
+output "redis_name" {
+  value = azurerm_redis_cache
 }
